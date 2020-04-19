8f9c9cef08d0b91713642c9cd6ae42c80a816af2
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2020-04-19 15:43:45 -0700

    Add ability to get file contents from Aioli

--- aioli.js
+++ aioli.js
@@ -150,12 +150,15 @@ class Aioli
         return this.send("exec", command);
     }
 
-    // List files and folders
+    // File system operations
     ls(path="/")
     {
         return this.send("ls", path);
     }
-
+    cat(path)
+    {
+        return this.send("cat", path);
+    }
 
     // =========================================================================
     // Worker Management: Track workers that Aioli is managing so that e.g. it
