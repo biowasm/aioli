834b9aa2ef4e5deb33fac833267f9ebbd0803d35
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2021-07-19 13:44:25 -0700

    Checkpoint

--- src/main.js
+++ src/main.js
@@ -1,17 +1,33 @@
-import { simd, threads } from "wasm-feature-detect";
+import pkg from "../package.json";
 import * as Comlink from "comlink";
+import { simd, threads } from "wasm-feature-detect";
 
+// Constants
+const URL_CDN_ROOT = "https://cdn.biowasm.com/v2";
+const configDefault = {
+	urlRoot: URL_CDN_ROOT,
+	urlAioli: `${URL_CDN_ROOT}/aioli/${pkg.version}.aioli.worker.js`
+}
+
+// Class: 1 object = 1 worker; user can decide if they want tools running in separate threads or all of them in one
 export default class Aioli
 {
-	constructor(config)
+	constructor(tools, config=configDefault)
 	{
-		//
-	}
+		// Input validation
+		if(tools == null)
+			throw "Expecting array of tools as input to Aioli constructor.";
+		if(!Array.isArray(tools))
+			tools = [ tools ];
 
-	async init()
-	{
+		// Create the WebWorker
 		const worker = new Worker("./dist/aioli.worker.js");
-		const obj = Comlink.wrap(worker);
-		return obj;
+		const aioli = Comlink.wrap(worker);
+
+		// Update configuration
+		aioli.tools = tools;
+		Object.assign(aioli.config, config);
+
+		return aioli;
 	}
 }
--- src/worker.js
+++ src/worker.js
@@ -1,13 +1,34 @@
+import pkg from "../package.json";
 import * as Comlink from "comlink";
 
-const obj = {
-    counter: 0,
-    inc() {
-        this.counter++;
+
+const aioli = {
+    //
+    tools: [],
+    config: {
+        a: 42,
+        dirData: "",
     },
-    mount() {
+
+    //
+    async init()
+    {
+        console.log(aioli.tools);
+        console.log(aioli.config)
+        console.log(`aioli v${pkg.version}`)
+        return 345;
+    },
+
+    //
+    mount(files) {
         console.log("mount")
-    }
+        console.log(files[0].name)
+        console.log(files[0].size)
+        return 123
+    },
+    set(tools) {
+
+    },
 };
 
-Comlink.expose(obj);
+Comlink.expose(aioli);
