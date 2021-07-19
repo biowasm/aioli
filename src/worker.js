72066d65efb42a6d8fa84144f2d506dc4b631dad
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2021-07-19 15:25:10 -0700

    Allow user to execute commands and figure out automatically which tool is the right one to use

--- src/worker.js
+++ src/worker.js
@@ -56,28 +56,64 @@ const aioli = {
 				printErr: text => tool.stderr += `${text}\n`
 			});
 
-			// Initialize some variables
+			// Initialize variables
 			tool.stdout = "";
 			tool.stderr = "";
-			aioli[tool.program] = tool;
 		}
 
-		console.log(aioli.tools[0].module.FS.readdir("/"));
+		// console.log(aioli.tools[0].module.FS.readdir("/"));
 		// console.log(aioli.config)
 		// console.log(`aioli v${pkg.version}`)
 		return 345;
 	},
 
-	//
+	// =========================================================================
+	// Mount files to the virtual file system
+	// =========================================================================
+	// TODO:
 	mount(files) {
 		console.log("mount")
 		console.log(files[0].name)
 		console.log(files[0].size)
 		return 123
 	},
-	set(tools) {
 
+	// =========================================================================
+	// Execute a command
+	// =========================================================================
+	async exec(command)
+	{
+		// Input validation
+		if(!command)
+			throw "Expecting a command";
+		// Extract tool name 
+		const args = command.split(" ");
+		const toolName = args.shift();
+
+		// Does it match a program we've already loaded?
+		const tools = aioli.tools.filter(d => d.program == toolName);
+		if(tools.length == 0)
+		throw `Program ${toolName} not found.`;
+		// Prepare tool
+		const tool = tools[0];		
+		tool.stdout = "";
+		tool.stderr = "";
+		
+		// Run command. Stdout/Stderr will be saved to "tool.stdout"/"tool.stderr" (see "print" and "printErr" above)
+		tool.module.callMain(args);
+
+		// TODO:
+		// Re-open stdout/stderr (fix error "error closing standard output: -1")
+		tool.module.FS.streams[1] = tool.module.FS.open("/dev/stdout", "w");
+		tool.module.FS.streams[2] = tool.module.FS.open("/dev/stderr", "w");
+
+		return {
+			stdout: tool.stdout,
+			stderr: tool.stderr
+		}
 	},
+	//
+	// TODO: ls, cat, download
 };
 
 Comlink.expose(aioli);
