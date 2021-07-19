d32d1db5caf5968c8e826dab2750679da8d08a1c
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2021-07-19 13:43:33 -0700

    Enable importing JSON files so we download the correct Aioli worker version based on what is in package.json!

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
-import resolve from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
-import terser from "rollup-plugin-terser";
+import resolve from "@rollup/plugin-node-resolve";  // Resolve node import statements
+import commonjs from "@rollup/plugin-commonjs";     // ES module conversion
+import terser from "rollup-plugin-terser";          // Minify JS to save space
+import json from "@rollup/plugin-json";             // Allow us to import JSON from main.js
 import pkg from "./package.json";
 
 export default [
@@ -12,7 +13,7 @@ export default [
 			file: pkg.browser,
 			format: "umd"
 		},
-		plugins: [ resolve(), commonjs(), terser.terser() ]
+		plugins: [ resolve(), commonjs(), terser.terser(), json() ]
 	},
 	// WebWorker
 	{
@@ -20,6 +21,6 @@ export default [
 		output: {
 			file: pkg.worker,
 		},
-		plugins: [ resolve(), commonjs(), terser.terser() ]
+		plugins: [ resolve(), commonjs(), terser.terser(), json() ]
 	}
 ];
